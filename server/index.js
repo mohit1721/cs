b5ba132854f6f2f22d664ed1515cab150dc09e98
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2024-09-25 00:35:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const io = new Server(server);
 dotenv.config();//load dotenv config
 app.use(
   cors({
-    origin:[ "http://localhost:8000", "http://localhost:5000","https://codes-sync.vercel.app/",""],//"*",  //FOR FRONTEND..//  methods: ["GET", "POST", "PUT", "DELETE"],..VVI..to entertain frontend req.[[http://localhost:3000]] -->:["http://localhost:3000","https://mystudynotion.vercel.app","https://study1-jlkmw7ckr-mohit1721s-projects.vercel.app"], --------------------------["https://mystudynotion.vercel.app"]  
+    origin:[ "http://localhost:8000", "http://localhost:5000","https://codes-sync.vercel.app/","https://cs-tau.vercel.app/"],//"*",  //FOR FRONTEND..//  methods: ["GET", "POST", "PUT", "DELETE"],..VVI..to entertain frontend req.[[http://localhost:3000]] -->:["http://localhost:3000","https://mystudynotion.vercel.app","https://study1-jlkmw7ckr-mohit1721s-projects.vercel.app"], --------------------------["https://mystudynotion.vercel.app"]  
     credentials: true,
   })
 );
